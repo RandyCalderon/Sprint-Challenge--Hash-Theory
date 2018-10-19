6c7a66a828953c50bdfce079b66f59fe8145c556
Randy Calderon <randycweb@gmail.com>
2018-10-19 10:03:19 -0700

    Added ex1

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,6 +1,20 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+  weight = {}
+
+  #for every item weight in the weights list 
+  for i in range(len(weights)):
+    # if the weights list is not found in the dictionary
+    if weights[i] not in weight:
+      weight[weights[i]] = i
+
+    include = limit - weights[i]
+
+    if include in weight and weight[include] != i:
+      return (i, weight[include])
+  return ()
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
   pass 
+
+# Store each weight's list index as the value for example weight = index[2]   
