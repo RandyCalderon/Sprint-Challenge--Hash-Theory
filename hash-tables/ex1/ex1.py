3238e30b6ec6734ce2e1084804efed4b2f7e4aab
Randy Calderon <randycweb@gmail.com>
2018-10-19 10:29:06 -0700

    Added comments to ex1 and following through with my solution

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,4 +1,5 @@
 def get_indices_of_item_weights(weights, limit):
+  #declare dictionary - figure out a better name
   weight = {}
 
   #for every item weight in the weights list 
@@ -7,10 +8,14 @@ def get_indices_of_item_weights(weights, limit):
     if weights[i] not in weight:
       weight[weights[i]] = i
 
+    # Find the acceptable weight that doesn't exceed the set limit
     include = limit - weights[i]
 
+    # The condition to return the result as a tuple
     if include in weight and weight[include] != i:
       return (i, weight[include])
+
+    # Returns an empty tuple if pair isn't found
   return ()
 
 if __name__ == '__main__':
