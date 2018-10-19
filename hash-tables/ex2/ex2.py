858eb41b49561f71544ae6011a8f415545c0b5b2
Randy Calderon <randycweb@gmail.com>
2018-10-19 15:24:13 -0700

    Added ex2

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,4 +1,20 @@
 def reconstruct_trip(tickets):
+
+  trips = dict(tickets)
+
+  source = None
+
+  trip = []
+
+  for i in range(len(tickets)):
+    if source in places:
+      if places[source] is not None:
+        trip.append(places[source])
+        source = places[source]
+      else: 
+        return []
+  
+  return trip
   pass 
 
 if __name__ == '__main__':
